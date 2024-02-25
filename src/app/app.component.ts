3bf3ffe7ad059d5c81ae2ebb0e225397344f3b9a
PC <luciomuller81@gmail.com>
2024-02-25 15:34:25 -0300

    works for now

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';  // Import CommonModule
 import { RouterOutlet } from '@angular/router';
 import { Wishitem } from '../shared/models/wishitem';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],  // Add CommonModule to the imports array
+  imports: [CommonModule,FormsModule],  // Add CommonModule to the imports array
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']  // Fix the typo in styleUrls
 })
@@ -25,8 +26,11 @@ export class AppComponent {
     item.isComplete = !item.isComplete;
     console.log(item);
   }
+  deleteElement(item : Wishitem, index : number) {
+    this.items.splice(index,1);
+    }
   addNewWish(){
-    // add a wish
-    //clear a texbox
+    this.items.push(new Wishitem(this.newWishText));
+    this.newWishText = '';
   }
 }
